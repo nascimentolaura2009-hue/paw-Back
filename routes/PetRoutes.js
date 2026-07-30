795aace238a2e6091a2c340fc851aebd144e3a87
Rhaynana Oliveira Antunes <rhaynanoliveiraantunes@gmail.com>
2026-07-30 15:38:18 -0300

    fix: correct router export order in PetRoutes.js

--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -1,3 +1,18 @@
-import router from "./PetRoutes.js";
+import express from "express";
+import {
+  createPet,
+  getAllPets,
+  getPetById,
+  updatePet,
+  deletePet,
+} from "../controllers/petController.js";
+
+const router = express.Router();
+
+router.post("/", createPet);
+router.get("/", getAllPets);
+router.get("/:id", getPetById);
+router.put("/:id", updatePet);
+router.delete("/:id", deletePet);
 
 export default router;
